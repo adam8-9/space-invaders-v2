fe9e68b7d02fb06e452d62da87797d064d5991fa
Adam <31065348+adam8-9@users.noreply.github.com>
2021-12-19 20:12:52 +0000

    - added in extra alien bullets - when alien ship shooting dies the ship behind it will shoot - fixed logic for first bullets to stop shooting after alien dies

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ function draw() {
     for (let i = 0; i < alienInvaders.length; i++) {
         if (!aliensRemoved.includes(i)) {
             squares[alienInvaders[i]].classList.add('invader')
+            //console.log(alienInvaders[i], squares[alienInvaders[i]])
         }
     }
 }
@@ -42,6 +43,7 @@ squares[currentShooterIndex].classList.add('shooter')
 
 function moveShooter(e) {
     squares[currentShooterIndex].classList.remove('shooter')
+
     switch (e.key) {
         case 'ArrowLeft':
             if (currentShooterIndex % width !== 0) currentShooterIndex -= 1
@@ -58,7 +60,8 @@ document.addEventListener('keydown', moveShooter)
 
 function moveInvaders() {
     let leftEdge = alienInvaders[0] % width === 0
-    let rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1
+    let rightEdge = alienInvaders[alienInvaders.length - 1] % width === width - 1 // 40 % 15 = 10,
+    //console.log(leftEdge, rightEdge)
     remove()
 
     if (rightEdge && goingRight) {
@@ -68,8 +71,17 @@ function moveInvaders() {
             direction = -1
             goingRight = false
         }
+        //console.log(alienInvaders)
 
     }
+
+    // const alienInvaders = [
+    //     0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    //     15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    //     30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+    // ]
+    [21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60]
+
 
     if (leftEdge && !goingRight) {
         for (let i = 0; i < alienInvaders.length; i++) {
@@ -82,6 +94,8 @@ function moveInvaders() {
 
     for (let i = 0; i < alienInvaders.length; i++) {
         alienInvaders[i] += direction
+        //console.log(alienInvaders[alienInvaders.length - 1], alienInvaders[alienInvaders.length - 1] % width)
+
     }
 
     draw()
@@ -92,6 +106,7 @@ function moveInvaders() {
         clearInterval(invadersId)
         clearInterval(alienBullet)
         clearInterval(moveLaser)
+        // console.log(check)
     }
 
     alienInvaders.forEach((invader, index) => {
@@ -108,13 +123,14 @@ function moveInvaders() {
         check = false
         clearInterval(invadersId)
         clearInterval(moveLaser)
+        clearInterval(alienBullet)
     }
 }
 
-invadersId = setInterval(moveInvaders, 100)
+invadersId = setInterval(moveInvaders, 3000)
 
 function shoot(e) {
-    console.log(e.key)
+    //  console.log(e.key)
     let laserId
     let currentLaserIndex = currentShooterIndex
     function moveLaser() {
@@ -147,46 +163,89 @@ function shoot(e) {
             break
     }
 }
+document.addEventListener('keydown', shoot)
+//laserId = setInterval(shoot, 100)
 
-let alienBullet
+let alienBulletId
 function alienShoot() {
-    let currentAlienLaser = 30
+    // let 
+    //let laser1Positions = [alienInvaders[20], alienInvaders[10], alienInvaders[0]]
+    let laser1 = alienInvaders[20]
+    let laser2 = alienInvaders[10]
+    let laser3 = alienInvaders[0]
+
+    // let currentLaser1Indexes = [20, 10, 0]
+    //let currentLaser1Index = currentLaser1Indexes[0];
+    // let AlienLaser2 = alienInvaders[20]
+    // let AlienLaser3 = alienInvaders[20]
     function enemyLaser() {
 
-        if (alienInvaders.length >= 20 && currentAlienLaser > 225) {
-            // let alienLaser1 = Math.floor(Math.random() * (39 - 30 + 1) + 30)
-            let alienLaser1 = 30
+        // if (alienInvaders.length >= 20) {
+        //     // let alienLaser1 = Math.floor(Math.random() * (39 - 30 + 1) + 30)
+        //     let alienLaser1 = 35
 
-            currentAlienLaser += alienInvaders.indexOf(alienLaser1)
-            //  console.log(alienLaser1, currentAlienLaser, squares[alienLaser1])
+        //     currentAlienLaser += alienInvaders.indexOf(alienLaser1)
+        //     //  console.log(alienLaser1, currentAlienLaser, squares[alienLaser1])
+        // }
+        // let iterator1 = laser1Positions[Symbol.iterator]()
+        // let iterator2 = currentLaser1Indexes[Symbol.iterator]()
+        if (!aliensRemoved.includes(20)) {
+            if (laser1 < 225) {
+                squares[laser1].classList.remove('laser')
+                laser1 += width
+                squares[laser1].classList.add('laser')
+            } else {
+                laser1 = alienInvaders[20]
+            }
         }
 
-
-        if (currentAlienLaser < 225) {
-            squares[currentAlienLaser].classList.remove('laser')
-            currentAlienLaser += width
-            // console.log(currentAlienLaser)
-            squares[currentAlienLaser].classList.add('laser')
+        if (aliensRemoved.includes(20) && !aliensRemoved.includes(10)) {
+            if (laser2 < 225) {
+                squares[laser2].classList.remove('laser')
+                laser2 += width
+                squares[laser2].classList.add('laser')
+            } else {
+                //alienInvaders[20]
+                //console.log(laser1 - (laser1))
+                laser2 = alienInvaders[10]
+            }
         }
 
+        if (aliensRemoved.includes(10) && !aliensRemoved.includes(0)) {
+            if (laser3 < 225) {
+                squares[laser3].classList.remove('laser')
+                laser3 += width
+                squares[laser3].classList.add('laser')
+            } else {
+                //alienInvaders[20]
+                //console.log(laser1 - (laser1))
+                laser3 = alienInvaders[0]
+            }
+        }
+        // } else {
+        //     currentLaser1Index = iterator2.next().value
+        //     laser1 = iterator1.next().value.position
+        // }
+        //  console.log()
         // check if alienInvaders.length is bigger than 20
         //then  choose 3(for now just 1) random indexes from the last 10 indexes and add laser class 
         //
         //  console.log(check)
+        // console.log(check)
     }
 
-    switch (check) {
-        case true:
-            alienBullet = setInterval(enemyLaser, 100)
-            break
-        case false:
-            alienBullet = clearInterval(enemyLaser)
-            break
-    }
+
+    setInterval(enemyLaser, 100)
+    //else clearInterval(enemyLaser)
+
+
+
 }
-alienBullet = setInterval(alienShoot(check), 100)
+
+alienBulletId = setInterval(alienShoot(), 100)
+if (check === false) { clearInterval(alienBulletId) }
 
 
 
-document.addEventListener('keydown', shoot)
+
 
